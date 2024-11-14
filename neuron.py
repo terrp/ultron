6a926eaaa1cd0b1f4cd8abf0decde93009a25c00
terp <andrewterp21@gmail.com>
2024-11-14 16:55:42 -0500

    Added Network class and test

--- neuron.py
+++ neuron.py
@@ -7,9 +7,47 @@ def sigmoid(x):
 class Neuron:
     def __init__(self, weights, bias):
         self.weights = weights
-        self.bias
+        self.bias = bias
 
     def feedforward(self, inputs):
         # Weight inputs, add bias, then use activation function in sigmoid
         total = np.dot(self.weights, inputs) + self.bias
         return sigmoid(total)
+
+weights = np.array([0, 1]) # w1 = 0, w2 = 1
+bias = 4                   # b = 4
+n = Neuron(weights, bias)
+
+x = np.array([2, 3])       # x1 = 2, x2 = 3
+print(n.feedforward(x))    # 0.9990889488055994
+
+class NeuralNetwork:
+    '''
+    A neural network with
+        2 inputs
+        a hidden layer with 2 neurons (h1, h2)
+        an output layer with one neuron (o1)
+    Each neuron has the same weights and bias:
+        - w = [0, 1]
+        - b = 0
+    '''
+    def __init__(self):
+        weights = np.array([0, 1])
+        bias = 0
+
+        # The Neuron class here is from the previous section
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+
+    def feedforward(self, x):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+
+        # The inputs for o1 are the  outputs from h1 and h2
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+        return out_o1
+    
+network = NeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x)) 
