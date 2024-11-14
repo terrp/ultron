67fa37d36bef921c7d03675d2e8fc41380916e4c
terp <andrewterp21@gmail.com>
2024-11-14 16:32:44 -0500

    added neuron class and activation function

--- neuron.py
+++ neuron.py
@@ -1,2 +1,15 @@
 import numpy as np
 
+def sigmoid(x):
+    # Activation function: f(x) = 1 / (1 + e^(-x))
+    return 1 / (1 + np.exp(-x))
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias
+
+    def feedforward(self, inputs):
+        # Weight inputs, add bias, then use activation function in sigmoid
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
